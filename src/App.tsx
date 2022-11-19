82eb80bc51733e63162ede42d499066bd4e1b82b
wallas5h@gmail.com <wallas5h@gmail.com>
2022-11-19 11:13:49 +0100

    add store to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import "./App.css";
 import { Navigation } from "./components";
+import { persistor, store } from "./features/store";
 import { Routing } from "./Routing";
 
 const App = () => {
   return (
-    <>
-      <Navigation />
-      <Routing />
-    </>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Navigation />
+        <Routing />
+      </PersistGate>
+    </Provider>
   );
 };
 
