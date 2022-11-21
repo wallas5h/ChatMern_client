5d2c069dbdd018d8a6c34b674fcd0ac3f92c2bfd
wallas5h@gmail.com <wallas5h@gmail.com>
2022-11-21 08:05:15 +0100

    add api socket url

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,1 +1,4 @@
-export const apiUrl = process.env.REACT_APP_API_URL ?? "http://localhost:3001/api";
+export const apiUrl =
+  process.env.REACT_APP_API_URL ?? "http://localhost:3001/api";
+export const apiSocketUrl =
+  process.env.REACT_APP_API_URL ?? "http://localhost:3001";
