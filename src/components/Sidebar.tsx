761fd8519d90da934dfb8cad191ebb21f176e2c1
wallas5h@gmail.com <wallas5h@gmail.com>
2022-11-27 18:27:47 +0100

    update Sitebar component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import { ListGroup, ListGroupItem } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { apiUrl } from "../config/api";
-import { User } from "../dto/user";
+import { PrivateUser } from "../dto/user";
 import {
   setCurrentRoom,
   setMembers,
@@ -12,10 +12,10 @@ import {
   socket,
 } from "../features/appSlice";
 import { RootState } from "../features/store";
-import { resetNotifications } from "../features/userSlice";
+import { addNotifications, resetNotifications } from "../features/userSlice";
 
 export const Sidebar = () => {
-  const { rooms, members, currentRoom } = useSelector(
+  const { rooms, members, currentRoom, privateMemberMsg } = useSelector(
     (state: RootState) => state.app
   );
   const user = useSelector((state: RootState) => state.user);
@@ -26,8 +26,10 @@ export const Sidebar = () => {
       getRooms();
       getMembers();
       dispatch(setCurrentRoom("general"));
+      dispatch(setPrivateMemberMSg(null));
       socket.emit("join-room", "general");
       socket.emit("new-user");
+      console.log(user);
     }
   }, []);
 
@@ -66,6 +68,24 @@ export const Sidebar = () => {
     dispatch(resetNotifications(room));
   };
 
+  socket.off("notifications").on("notifications", (room) => {
+    if (currentRoom != room) dispatch(addNotifications(room));
+  });
+
+  const orderId = (id1: string, id2: string) => {
+    if (id1 > id2) {
+      return id1 + "-" + id2;
+    } else {
+      return id2 + "-" + id1;
+    }
+  };
+
+  const handlePrivateMemberMsg = (member: PrivateUser) => {
+    dispatch(setPrivateMemberMSg(member));
+    const roomId = orderId(String(user.id), String(member?._id));
+    joinRoom(roomId, false);
+  };
+
   if (!user.id) {
     return <></>;
   }
@@ -79,19 +99,36 @@ export const Sidebar = () => {
             className=" cursor-pointer hover:bg-green-300"
             key={index}
             onClick={() => joinRoom(room)}
+            active={room == currentRoom}
           >
-            {room}
+            {room}{" "}
+            {currentRoom !== room && (
+              <span className="badge rounded-pill bg-primary">
+                {user.newMessages[room]}
+              </span>
+            )}
           </ListGroupItem>
         ))}
       </ListGroup>
       <h2 className="my-2 mt-4">Members</h2>
       <ListGroup>
-        {members.map((member: User, index) => (
+        {members.map((member: PrivateUser, index) => (
           <ListGroupItem
             className=" cursor-pointer hover:bg-green-300"
             key={index}
+            active={privateMemberMsg?._id === member?._id}
+            onClick={() => handlePrivateMemberMsg(member)}
+            disabled={member._id === user.id}
           >
-            {member.name}
+            <div className="flex items-center">
+              <img
+                src={member.image}
+                alt={member.name}
+                className="w-[20px] h-[20px] object-cover rounded-full mr-[10px]"
+              />
+              <p>{member.name}</p>
+              {member._id === user.id ? <p className="ml-3">(You)</p> : ""}
+            </div>
           </ListGroupItem>
         ))}
       </ListGroup>
